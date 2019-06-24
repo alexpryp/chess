de17e3ff6945be80a5bfaa911df4ffc4b1baa589
alexpryp <alexpryp@gmail.com>
2019-06-24 23:16:12 +0300

    Added comments to a script file

--- script.js
+++ script.js
@@ -1,21 +1,13 @@
 "use strict"
 
-document.onmousedown = function(e) {
-	var dragElement = e.target;
+let wrapper = document.querySelector('#wrapper');
+
+function pickElement(e) { 
+	let dragElement = e.target;
 
 	if(!dragElement.classList.contains('chessman')) return;
 
-	var coords, shiftX, shiftY;
-
-	startDrag(e.clientX, e.clientY);
-
-	document.onmousemove = function(e) {
-		moveAt(e.clientX, e.clientY);
-	};
-
-	dragElement.onmouseup = function() {
-		finishDrag();
-	};
+	let coords, shiftX, shiftY;
 
 	function startDrag(clientX, clientY) {
 		shiftX = clientX - dragElement.getBoundingClientRect().left;
@@ -29,6 +21,7 @@ document.onmousedown = function(e) {
 	}
 
 	function finishDrag() {
+		//end of transfer, go from fixed to absolute-coordinates
 		dragElement.style.top = parseInt(dragElement.style.top) + pageYOffset + 'px';
 		dragElement.style.position = 'absolute';
 
@@ -37,31 +30,53 @@ document.onmousedown = function(e) {
 	}
 
 	function moveAt(clientX, clientY) {
-		var newX = clientX - shiftX;
-		var newY = clientY - shiftY;
+		//new coords
+		let newX = clientX - shiftX;
+		let newY = clientY - shiftY;
 
-		var newBottom = newY + dragElement.offsetHeight;
 
+		//process the removal of the lower window border
+		//new lower bound of the element
+		let newBottom = newY + dragElement.offsetHeight;
+
+		//if a new lower bound sticks out of the window - let's scroll it
 		if (newBottom > document.documentElement.clientHeight) {
-			var docBottom = document.documentElement.getBoundingClientRect().bottom;
-			var scrollY = Math.min(docBottom - newBottom, 10);
+			//coordinate of the lower border of the document relative to the window
+			let docBottom = document.documentElement.getBoundingClientRect().bottom;
 
+			//scrollBy, if it is not limited, it can scroll beyond the current document boundary
+			//usually scroll by 10px
+			//but if the distance from newBottom to docBottom is less, then less
+			let scrollY = Math.min(docBottom - newBottom, 10);
+
+			//rounding errors with fully scrolled page
+			//can lead to a negative scrollY, which will mean scrolling up
+			//correct this error
 			if (scrollY < 0) scroll = 0;
 
 			window.scrollBy(0, scrollY);
 
+			//with a sharp movement of the mouse element can be moved strongly down
+			//if it has gone beyond the bottom of the document -
+			//move to the lowest possible position within the document
 			newY = Math.min(newY, documentElement.clientHeight - dragElement.offsetHeight);
 		}
 
+
+		// process the removal of the upper border of the window
 		if (newY < 0) {
-			var scrollY = Math.min(-newY, 10);
+			//scroll up by 10px or less if we are at the very top
+			let scrollY = Math.min(-newY, 10);
 			if (scrollY < 0) {
-				scrollY = 0;
+				scrollY = 0; //fix rounding error
 			}
 			window.scrollBy(0, -scrollY);
+			//with a sharp movement of the mouse, the element could "fly out" much up, fix it
 			newY = Math.max(newY, 0);
 		}
 
+		//pinch the screen horizontally
+		//there is no scrolling, everything is simple
 		if (newX < 0) {
 			newX = 0;
 		}
@@ -73,5 +88,20 @@ document.onmousedown = function(e) {
 		dragElement.style.top = newY + 'px';
 	}
 
+	startDrag(e.clientX, e.clientY);
+
+	document.onmousemove = function(e) {
+		moveAt(e.clientX, e.clientY);
+	};
+
+	dragElement.onmouseup = function() {
+		finishDrag();
+	};
+
+	//cancel the default action on mousedown (text selection, it is superfluous)
+	event.preventDefault()
 	return false;
 }
+
+
+wrapper.addEventListener('mousedown', pickElement);
